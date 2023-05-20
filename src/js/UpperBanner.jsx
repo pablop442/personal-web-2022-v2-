cf5a9f695d2c66e5aed263d4b783139502ad755c
pablop442 <89651989+pablop442@users.noreply.github.com>
2023-05-20 14:15:06 +0200

    changed about me text

--- src/js/UpperBanner.jsx
+++ src/js/UpperBanner.jsx
@@ -4,51 +4,52 @@ import BgImgReducedSize from "../img/BgImgReducedSize.jpg";
 import Typical from "react-typical";
 import UpperBtn from "./UpperBtn.jsx";
 let bgImg = {
-	background: `linear-gradient(0deg, rgba(10, 17, 40, 0.7), rgba(10, 17, 40, 0.7)), url(${BgImgReducedSize})`,
-	backgroundPosition: `bottom`,
-	backgroundSize: "cover",
-	backgroundRepeat: "no-repeat",
+  background: `linear-gradient(0deg, rgba(10, 17, 40, 0.7), rgba(10, 17, 40, 0.7)), url(${BgImgReducedSize})`,
+  backgroundPosition: `bottom`,
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
 };
 
 const UpperBanner = () => {
-	return (
-		<>
-			<section
-				className="text-light p-5 p-lg-0 pt-lg-5 text-center text-sm-start upper-banner bg-image"
-				style={bgImg}>
-				<div className="container">
-					<div className="d-sm-flex flex-column d-flex justify-content-center align-items-center ">
-						<h1 className="text-center">
-							<span className="open-text">Hello!</span>
-							<br /> I'm <b>Pablo Peña</b> <br />
-						</h1>
-						<p className="text-start">Web Developer</p>
-						<UpperBtn />
-						<div className="my-5 ps-3  border-start">
-							<Typical
-								steps={[
-									"HTML5",
-									1000,
-									"CSS",
-									1000,
-									"JavaScript",
-									1000,
-									"Python",
-									1000,
-									"HTTP/Rest",
-									1000,
-									"Git",
-									1000,
-								]}
-								loop={Infinity}
-								wrapper="h2"
-							/>
-						</div>
-					</div>
-				</div>
-			</section>
-		</>
-	);
+  return (
+    <>
+      <section
+        className="text-light p-5 p-lg-0 pt-lg-5 text-center text-sm-start upper-banner bg-image"
+        style={bgImg}
+      >
+        <div className="container">
+          <div className="d-sm-flex flex-column d-flex justify-content-center align-items-center ">
+            <h1 className="text-center">
+              <span className="open-text">Hello!</span>
+              <br /> I'm <b>Pablo Peña</b> <br />
+            </h1>
+            <p className="text-start">Quality Assurance Engineer</p>
+            <UpperBtn />
+            <div className="my-5 ps-3  border-start">
+              <Typical
+                steps={[
+                  "HTML5",
+                  1000,
+                  "CSS",
+                  1000,
+                  "JavaScript",
+                  1000,
+                  "Python",
+                  1000,
+                  "HTTP/Rest",
+                  1000,
+                  "Git",
+                  1000,
+                ]}
+                loop={Infinity}
+                wrapper="h2"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default UpperBanner;
